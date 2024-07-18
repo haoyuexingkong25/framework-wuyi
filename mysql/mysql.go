0c438828c3d91069c4925493cdeda6e7721f8637
wuyi <13579@qq.com>
2024-07-18 17:09:45 +0800

    3

--- mysql/mysql.go
+++ mysql/mysql.go
@@ -2,13 +2,19 @@ package mysql
 
 import (
 	"fmt"
+	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-func MysqlClient(user, pass, hort, port, dbname string, handler func(db *gorm.DB) error) error {
+func MysqlClient(handler func(db *gorm.DB) error) error {
 	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local",
-		user, pass, hort, port, dbname,
+		//user, pass, hort, port, dbname,
+		viper.GetString("mysql.user"),
+		viper.GetString("mysql.pass"),
+		viper.GetString("mysql.hort"),
+		viper.GetString("mysql.port"),
+		viper.GetString("mysql.dbname"),
 	)
 	cli, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -21,5 +27,17 @@ func MysqlClient(user, pass, hort, port, dbname string, handler func(db *gorm.DB
 	}
 	defer db.Close()
 
+	fmt.Println("mysqlOk")
 	return handler(cli)
 }
+
+func BeginClient() {
+	//dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local",
+	//	user, pass, hort, port, dbname,
+	//)
+	//db.Begin()
+}
+
+//func ()  {
+//
+//}
